c623b87fce764972c482baec3371db282c360b75
pandeleche <4fcojavierceldranmartinez@gmail.com>
2018-02-28 10:44:50 +0100

    Añadido botones de inicio y comenzada la pantalla de creación de personaje

--- app/src/main/java/com/example/curro/seven/Inicio.java
+++ app/src/main/java/com/example/curro/seven/Inicio.java
@@ -1,13 +1,28 @@
 package com.example.curro.seven;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Inicio extends AppCompatActivity {
-
+    Button nueva_partida;
+    Button introducir_codigo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        nueva_partida =  findViewById(R.id.nueva_partida);
+        introducir_codigo = findViewById(R.id.introducir_codigo);
+        nueva_partida.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), introduccion.class);
+                startActivity(i);
+            }
+        });
     }
+
 }
