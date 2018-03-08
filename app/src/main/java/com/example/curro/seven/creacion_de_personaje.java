b0f811abf650ed91518b7ef91a5bc6196e6ead52
Pandeleche <curromakinon@hotmail.com>
2018-03-08 14:46:13 +0100

    Añadido botones de inicio y comenzada la pantalla de creación de personaje

--- app/src/main/java/com/example/curro/seven/creacion_de_personaje.java
+++ app/src/main/java/com/example/curro/seven/creacion_de_personaje.java
@@ -1,13 +1,45 @@
 package com.example.curro.seven;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class creacion_de_personaje extends AppCompatActivity {
+    Button confirmar_don;
+    TextView don_actual;
+    public static final String MyPREFERENCES = "MyPrefs" ;
+    SharedPreferences sharedpreferences;
+
+    enum Dones{
+        SABIDURIA, CIENCIA
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creacion_de_personaje);
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        confirmar_don = findViewById(R.id.confirmar_don);
+        don_actual = findViewById(R.id.don_actual);
+        don_actual.setText(R.string.sabiduria);
+        confirmar_don.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.putString("Don",don_actual.getText().toString());
+                editor.commit();
+
+                Intent i = new Intent(getApplicationContext(), introduccion.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
